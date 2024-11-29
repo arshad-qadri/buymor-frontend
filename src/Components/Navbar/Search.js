d7bdfce0ee9cb098e9ec3df544365eed837e8a9e
arshad <arshadqadri321@gmail.com>
2024-11-29 10:53:18 +0530

    search box fixs

--- src/Components/Navbar/Search.js
+++ src/Components/Navbar/Search.js
@@ -4,11 +4,11 @@ import { IoIosSearch } from "react-icons/io";
 const Search = () => {
   return (
     <div className="flex-grow mx-4 ">
-      <div className="relative  w-max">
+      <div className="relative  w-[500px]   border border-gray-300 rounded-md">
         <input
           type="text"
           placeholder="Search for products..."
-          className="w-[500px] max-w-md pl-4 pr-16 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-indigo-600 focus:outline-none"
+          className="w-full max-w-md pl-4 pr-16 py-2 border-0 rounded-md  focus:border-none focus:outline-none"
         />
         <button className="absolute top-1/2 right-1 -translate-y-1/2 h-full w-10 flex justify-center items-center">
           <IoIosSearch size={"20px"} />
