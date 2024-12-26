864a4f9fd0762195d062379a6b852c18ce25f32e
arshad <arshadqadri321@gmail.com>
2024-12-26 12:02:53 +0530

    uncomentted

--- src/variable.js
+++ src/variable.js
@@ -1,2 +1,2 @@
-// export const baseUrl = "https://buymor-backend.vercel.app/api"
-export const baseUrl = "http://localhost:5000/api"
+export const baseUrl = "https://buymor-backend.vercel.app/api"
+// export const baseUrl = "http://localhost:5000/api"
